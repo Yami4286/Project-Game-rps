7d8eb130139812f7b5e5713d0d522d6e490f21b6
Yami4286 <abdulhanan8974@gmail.com>
2025-08-17 17:26:15 +0500

    Made some changes till last time , made it look more good in sense so cpu now shows what it has chosen and a little tidy, i guess

--- java.js
+++ java.js
@@ -4,6 +4,7 @@ function ComputerChoice(){
     let min = 1;
     let max = 4;
    let ans = Math.floor(Math.random()*(max-min)) + min;
+   pc(ans);
    return ans;
    // 1 reserve for the rock , 2 reserve for paper , 3 reserve for scissors
 }
@@ -13,40 +14,44 @@ let Choice = prompt(" Rock \n  paper \n scissors \nEnter Your Choice");
     console.log("Your choice is: " + Choice);
    let cho =  Choice.toUpperCase();
   if(cho == "ROCK"){
-    console.log("1 & 2");
   return 1;
   } else if (cho == "PAPER") {
-    console.log("3 & 4");
    return 2;
   } else if( cho == "SCISSORS"){
-    console.log("5& 6");
    return 3;''
   }
    
 }
 
+function pc( num){
+     if (CpuChoice == 1){console.log("Cpu choice is: Rock")}else 
+        if (CpuChoice == 2){console.log("Cpu choice is: Paper")}else
+             if (CpuChoice == 3){console.log("Cpu Choice is Scissors")}
+}
+
 function PlayRound( hs, ps){
 const HumChoice = PlayerChoice();
 const CpuChoice= ComputerChoice();
- console.log(HumChoice +" " + CpuChoice);
+
  if(HumChoice == 1 && CpuChoice == 2){
+    
   console.log("Paper beats rock, Cpu wins");
-   return ps +1;
+ return ++ps;
  } else if(HumChoice == 2 && CpuChoice == 3 ){
   console.log("Scissors beat Paper, Cpu Wins");
-  return ps +1;
+ return ++ps;
  } else if(HumChoice == 3 && CpuChoice == 1){
   console.log("Rock beats scissors, Cpu wins");
-  return ps +1;
+  return ++ps;
  } else  if(HumChoice == 1 && CpuChoice == 3 ){
   console.log("Paper beats rock, Player wins");
-  return hs +1;
+   return ++hs;
  } else if(HumChoice == 2 && CpuChoice == 1){
   console.log("Paper beats rock, Player wins");
-   return hs +1;
+ return ++hs ;
  } else if(HumChoice == 3 && CpuChoice == 2){
   console.log("Scissors beats Paper, Player wins");
-   return hs +1;
+   return ++hs;
  } else if(HumChoice == CpuChoice){
     console.log("Draw");
     return hs, ps;
@@ -57,4 +62,4 @@ const ps = 0;
 
 PlayRound();
 
-console.log("Player Score: " + hs + "Cpu SCore: " + ps);
+console.log("Player Score: " + hs + " Cpu SCore: " + ps);
