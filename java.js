824e9bd8561d18221ce991028c66f0c2cddb3b63
Yami4286 <abdulhanan8974@gmail.com>
2025-08-17 17:14:43 +0500

    Project is almost finished , Everything is done NOw lets hope it runs correctly.

--- java.js
+++ java.js
@@ -2,10 +2,10 @@ console.log("Hello World");
 
 function ComputerChoice(){
     let min = 1;
-    let max = 7;
+    let max = 4;
    let ans = Math.floor(Math.random()*(max-min)) + min;
    return ans;
-   // 1 && 2 reserve for the rock , 3 && 4 reserve for paper 5 &&6 reserve for scissors
+   // 1 reserve for the rock , 2 reserve for paper , 3 reserve for scissors
 }
 
 function PlayerChoice(){
@@ -17,10 +17,10 @@ let Choice = prompt(" Rock \n  paper \n scissors \nEnter Your Choice");
   return 1;
   } else if (cho == "PAPER") {
     console.log("3 & 4");
-   return 3;
+   return 2;
   } else if( cho == "SCISSORS"){
     console.log("5& 6");
-   return 5;
+   return 3;''
   }
    
 }
@@ -29,8 +29,32 @@ function PlayRound( hs, ps){
 const HumChoice = PlayerChoice();
 const CpuChoice= ComputerChoice();
  console.log(HumChoice +" " + CpuChoice);
+ if(HumChoice == 1 && CpuChoice == 2){
+  console.log("Paper beats rock, Cpu wins");
+   return ps +1;
+ } else if(HumChoice == 2 && CpuChoice == 3 ){
+  console.log("Scissors beat Paper, Cpu Wins");
+  return ps +1;
+ } else if(HumChoice == 3 && CpuChoice == 1){
+  console.log("Rock beats scissors, Cpu wins");
+  return ps +1;
+ } else  if(HumChoice == 1 && CpuChoice == 3 ){
+  console.log("Paper beats rock, Player wins");
+  return hs +1;
+ } else if(HumChoice == 2 && CpuChoice == 1){
+  console.log("Paper beats rock, Player wins");
+   return hs +1;
+ } else if(HumChoice == 3 && CpuChoice == 2){
+  console.log("Scissors beats Paper, Player wins");
+   return hs +1;
+ } else if(HumChoice == CpuChoice){
+    console.log("Draw");
+    return hs, ps;
+ }
 }
 const  hs = 0;
 const ps = 0;
 
 PlayRound();
+
+console.log("Player Score: " + hs + "Cpu SCore: " + ps);
